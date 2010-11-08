792b1c72f641ca9f31b62339edc2f9b45c703a92
Owen Jacobson <owen.jacobson@grimoire.ca>
2010-11-08 10:41:51 -0500

    The example consumer allows printing the PID of the worker process.

--- sparkplug/examples.py
+++ sparkplug/examples.py
@@ -1,10 +1,12 @@
+import os
+
 class EchoConsumer(object):
     def __init__(self, channel, format):
         self.channel = channel
         self.format = format
     
     def __call__(self, msg):
-        print self.format % {'body': msg.body}
+        print self.format % {'body': msg.body, 'pid': os.getpid()}
         self.channel.basic_ack(msg.delivery_tag)
 
 class Broken(object):
