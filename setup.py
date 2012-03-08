36d03db0ecf84bcc89a435be2593123ba2230f9f
Owen Jacobson <owen.jacobson@grimoire.ca>
2012-03-08 14:59:52 -0500

    Updated version number for release (1.3).

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sparkplug',
-    version='1.3.dev',
+    version='1.3',
     author='Owen Jacobson',
     author_email='owen.jacobson@grimoire.ca',
     url='http://alchemy.grimoire.ca/python/sites/sparkplug/',
