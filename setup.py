315cd703fc77c52c6c73e84a8276bb0cad5a1b9e
Owen Jacobson <owen.jacobson@grimoire.ca>
2013-09-09 13:55:09 -0400

    Updated version number for development (1.5.dev)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(p.join(p.dirname(__file__), 'requirements.txt'), 'r') as reqs:
 
 setup(
     name='sparkplug',
-    version='1.4',
+    version='1.5.dev',
     author='Owen Jacobson',
     author_email='owen.jacobson@grimoire.ca',
     url='http://alchemy.grimoire.ca/python/sites/sparkplug/',
